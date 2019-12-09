7d9dd8d6c06407bed356331f8d2be87845ef2845
only4ly <zhang9748@foxmail.com>
2019-12-09 23:19:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
+function transformPX(num) {
+  return `transfromPX(${num})`;
+}
+
+function isEndOfPX(value) {
+  return (value + "").indexOf("px") === value.length - 2;
+}
+
+function getPX(value) {
+  return value.slice(0,-2);
+}
+
+function transformPropValue(propName, propValue) {
+  if (shouldIgnoreOnReactNativeProp.indexOf(propName) > -1) {
+    return `...(process.env.TARO_ENV === 'rn' ? {${propName}: '${propValue}'}:null),`;
+  } else {
+    const isPX = isEndOfPX(propValue);
+
+    return `${propName}: ${isPX ? transformPX(getPX(propValue)) :'\''+ propValue+"'"},`;
+  }
+}
+
+const shouldIgnoreOnReactNativeProp = ["boxSizing", "whiteSpace", "textOverflow"];
+
+/**
+ *
+ * @param {React.CSSProperties} CSSProperties
+ */
+function transformCSSProperties(CSSProperties) {
+  let str = "{";
+  for (let key in CSSProperties) {
+    str += transformPropValue(key, CSSProperties[key]);
+  }
+
+  str += "}";
+
+  return str;
+}
+
+function transformStyle(styles) {
+  let str = "{";
+
+  for (let k in styles) {
+    if (styles.hasOwnProperty(k)) {
+     str= str + k + ":" + transformCSSProperties(styles[k]) + ",";
+    }
+  }
+
+  str += "}";
+  return str;
+}
+
 module.exports = function(schema, option) {
   const renderData = {};
   const style = {};
@@ -19,33 +71,33 @@ module.exports = function(schema, option) {
   }
 
   function transform(json) {
-    var result = '';
+    var result = "";
 
     if (Array.isArray(json)) {
       json.forEach(function(node) {
         result += transform(node);
       });
-    } else if (typeof json == 'object') {
+    } else if (typeof json == "object") {
       var type = json.componentName && json.componentName.toLowerCase();
       var className = json.props && json.props.className;
-      var classString = className ? ` style={styles.${className}}` : '';
+      var classString = className ? ` style={styles.${className}}` : "";
 
       switch (type) {
-        case 'text':
+        case "text":
           var innerText = parseProps(json.props.text);
-          result += `<span${classString}>${innerText}</span>`;
+          result += `<Text${classString}>${innerText}</Text>`;
           break;
-        case 'div':
-        case 'page':
+        case "div":
+        case "page":
           if (json.children && json.children.length > 0) {
-            result += `<div${classString}>${transform(json.children)}</div>`;
+            result += `<View${classString}>${transform(json.children)}</View>`;
           } else {
-            result += `<div${classString} />`;
+            result += `<View${classString} />`;
           }
           break;
-        case 'image':
+        case "image":
           var source = parseProps(json.props.src, true);
-          result += `<img${classString} src={${source}}  />`;
+          result += `<Image${classString} src={${source}}  />`;
           break;
       }
 
@@ -61,21 +113,21 @@ module.exports = function(schema, option) {
   var jsx = `${transform(schema)}`;
 
   renderData.modClass = `
-class Mod extends Component {
-  render() {
-    return (
-      ${jsx}
-    );
-  }
-}
+    class Mod extends Component {
+      render() {
+        return (
+          ${jsx}
+        );
+      }
+    }
   `;
 
-  renderData.style = `var styles = ${JSON.stringify(style)}`;
-  renderData.exports = `ReactDOM.render(<Mod />, document.getElementById('container'));`;
+  renderData.style = `var styles = ${(transformStyle(style))};`;
+  renderData.exports = `export default Mod;`;
 
   const prettierOpt = {
-    parser: 'babel',
-    printWidth: 120,
+    parser: "babel",
+    printWidth: 80,
     singleQuote: true
   };
 
@@ -83,4 +135,4 @@ class Mod extends Component {
     renderData: renderData,
     prettierOpt: prettierOpt
   };
-}
+};
